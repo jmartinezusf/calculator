301137b01ec3fa65657fd5631444bbffcc337d8b
Jordan Martinez <jmartinez10@usf.edu>
2022-03-31 21:41:28 -0400

    Created add, subtract, multiply, and divide function

---
+++ script.js
@@ -0,0 +1,19 @@
+function add() {
+    return a + b;
+}
+
+function subtract() {
+    return a - b;
+}
+
+function multiply() {
+    return a * b;
+}
+
+function divide() {
+    return a / b;
+}
+
+function operate(a, b) {
+
+}
